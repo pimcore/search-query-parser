f5bee80dda1e7924ecb7be9361dfd68506753a49
Mathias Geat <mathias.geat@elements.at>
2017-01-19 07:56:52 +0100

    Move zend-db-example to examples

--- examples/zend-db-query.php
+++ examples/zend-db-query.php
@@ -1,10 +1,9 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-$input = '!@mathias AND 1212 AND !foo OR (!(amya AND 12) blah) OR blubb';
-$input = 'mathias !(@5020)';
-
+$input = 'doe AND @1212 AND !foo OR (!(amya AND 12) blah) OR baz';
+$input = 'doe !@john';
 dump($input);
 
 $lexer = new \Query\Lexer();
@@ -14,6 +13,7 @@ dump($tokens);
 
 $parser = new \Query\Parser($tokens);
 $query = $parser->parse();
+
 dump($query);
 
 $db = new Zend_Db_Adapter_Pdo_Sqlite(array(
@@ -24,12 +24,10 @@ $select = $db
     ->select()
     ->from('foo');
 
-
 $queryBuilder = new \Query\QueryBuilder\ZendDbSelect([
     'foo',
     'bar'
 ]);
+
 $select = $queryBuilder->processQuery($select, $query);
-
 dump($select->__toString());
-
