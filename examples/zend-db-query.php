ada6c9d6f71898928617b42662d30b18cb220c05
Mathias Geat <mathias.geat@elements.at>
2017-01-19 08:34:13 +0100

    Annotate zend db example

--- examples/zend-db-query.php
+++ examples/zend-db-query.php
@@ -2,28 +2,33 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$input = 'doe AND "1212" AND !foo OR (!("amya" AND 12) blah) OR baz';
-//$input = 'doe !"john"';
+$input = 'doe AND "1212" AND !foo OR (!("amya" AND 12) blah) OR baz'; // complex query
+$input = 'john !5020 AND "foobar"'; // LIKE %john% AND NOT LIKE %5020% AND = foobar
 dump($input);
 
 $lexer = new \SearchQueryParser\Lexer();
 $tokens = $lexer->lex($input);
 
+// contains array of extracted tokens
 dump($tokens);
 
 $parser = new \SearchQueryParser\Parser();
 $query = $parser->parse($tokens);
 
+// contains the abstract Query object
 dump($query);
 
+// use the Zend_Db query builder to transform the Query into conditions
 $db = new Zend_Db_Adapter_Pdo_Sqlite(array(
     'dbname' => ':memory:'
 ));
 
+// dummy query
 $select = $db
     ->select()
     ->from('foo');
 
+// build conditions for every passed field
 $queryBuilder = new \SearchQueryParser\QueryBuilder\ZendDbSelect([
     'foo',
     'bar'
